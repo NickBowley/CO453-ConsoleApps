bd7680c5f785427f6caeceb3d7e6fb554ed4fdee
NickBowley <116365231+NickBowley@users.noreply.github.com>
2023-02-17 12:58:57 +0000

    Refactored input methods

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -4,50 +4,127 @@ using System.Runtime.CompilerServices;
 namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// This app currently performs three functions. It converts
+    /// Miles to feet, feet to miles and miles to metres.
     /// </summary>
     /// <author>
-    /// Nick Bowley version 0.1
+    /// Nick Bowley version 0.2
     /// </author>
     public class DistanceConverter
     {
-        public static double FEET_IN_MILE = 5280;
+        public const int FEET_IN_MILE = 5280;
+        public const double METRES_IN_MILES = 1609.34;
         private double miles;
         private double feet;
-        public void Run()
+        private double metres;
+
+        public void MilesToFeet()
 
         {
-            OutputHeading();
+            OutputHeading("Converting miles to feet");
             InputMiles();
             CalculateFeet();
-            //OutputFeet();
+            OutputFeet();
+        }
 
-            
+        public void FeetToMiles()
+        {
+            OutputHeading("Converting feet to miles");
+            InputFeet();
+            CalculateMiles();
+            OutputMiles();
+        }
+
+        public void MilesToMetres()
+        {
+            OutputHeading("Converting miles to metres");
+            InputMiles();
+            CalculateMetres();
+            OutputMetres();
         }
 
         private void InputMiles()
         {
             Console.WriteLine("Please enter the number of miles: ");
-            string input = Console.ReadLine();
-            miles = Convert.ToDouble(input);
-            Console.WriteLine("Mile entered is:" + miles);
+            string value = Console.ReadLine();
+            miles = Convert.ToDouble(value);
+
+        }
+
+        private void InputFeet()
+        {
+            Console.WriteLine("Please enter the number of feet: ");
+            string value = Console.ReadLine();
+            feet = Convert.ToDouble(value);
+
+        }
+
+        private void InputMetres()
+        {
+            Console.WriteLine("Please enter the number of metres: ");
+            string value = Console.ReadLine();
+            metres = Convert.ToDouble(value);
+
         }
 
         private void CalculateFeet()
         {
-            feet = miles * 5280;
+            feet = miles * FEET_IN_MILE;
+        }
+
+        private void CalculateMiles()
+        {
+            miles = feet / FEET_IN_MILE;
+        }
+
+        private void CalculateMetres()
+        {
+            metres = miles / FEET_IN_MILE;
         }
 
         private void OutputFeet()
         {
-            Console.WriteLine(miles + " miles is  + feet +  feet!");
+            Console.WriteLine(miles + " miles is " + feet + " feet!");
         }
 
-        private void OutputHeading()
+        private void OutputMiles()
         {
-            Console.WriteLine("----------------------------------");
-            Console.WriteLine("    App01 Distance Converter");
-            Console.WriteLine("----------------------------------");
+            Console.WriteLine(feet + " feet is " + miles + " miles!");
         }
+
+        private void OutputMetres()
+        {
+            Console.WriteLine(miles + " miles is " + metres + " metres!");
+        }
+
+
+
+        private void OutputHeading(String prompt)
+        {
+            Console.WriteLine("\n----------------------------------");
+            Console.WriteLine("    App01 Distance Converter");
+            Console.WriteLine("         By Nick Bowley     ");
+            Console.WriteLine("----------------------------------\n");
+
+            Console.WriteLine(prompt);
+            Console.WriteLine();
+        }
+
     }
+
 }
+
+        
+
+       
+
+        
+
+        
+
+     
+
+    
+    
+
+
